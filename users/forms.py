19b5af3d400647e54f46a62c23e96995bcb4218b
ANUJA19 <anujadhole2000@gmail.com>
2020-10-28 14:14:25 +0530

    v1

---
+++ users/forms.py
@@ -0,0 +1,40 @@
+from django import forms
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm,AuthenticationForm
+from .models import Profile
+
+class UserRegisterForm(UserCreationForm):
+    email  = forms.EmailField()
+
+    class Meta:
+        model = User
+        fields = ['username','email','password1','password2']
+
+
+class UserUpdateForm(forms.ModelForm):
+    email  = forms.EmailField()
+
+    class Meta:
+        model = User
+        fields = ['username','email']
+
+
+class ProfileUpdateForm(forms.ModelForm):
+    class Meta :
+        model = Profile
+        fields=['image']
+
+
+class UserLoginForm(AuthenticationForm):
+    def __init__(self, *args, **kwargs):
+        super(UserLoginForm, self).__init__(*args, **kwargs)
+
+    username = forms.EmailField(widget=forms.TextInput(
+        attrs={'class': 'form-control', 'placeholder': '', 'id': 'hello'}))
+    password = forms.CharField(widget=forms.PasswordInput(
+        attrs={
+            'class': 'form-control',
+            'placeholder': '',
+            'id': 'hi',
+        }
+))
